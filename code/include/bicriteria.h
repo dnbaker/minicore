5c407652976fcf08686bbc8539a4f828c17679ba
dnbaker <dnb@cs.jhu.edu>
2019-12-10 11:55:31 -0500

    Sampling finished. All that remains in bicriteria is Dijkstra lengths (|F| * |R| in cost) and removing edges.

--- code/include/bicriteria.h
+++ code/include/bicriteria.h
@@ -11,7 +11,7 @@ template<typename S>
 struct ThorupApprox {
     S container_;
     template<typename G>
-    auto thorup_sample(const G &x, unsigned k) {
+    auto thorup_sample(const G &x, unsigned k, uint64_t seed=0) {
         using Vertex = typename G::Vertex;
         // Algorithm E, Thorup p.418
         container_.clear();
@@ -22,34 +22,39 @@ struct ThorupApprox {
         size_t samples_per_round = std::ceil(21. * k * logn / eps);
         size_t iterations_per_round = std::ceil(3 * logn);
         flat_hash_set<Vertex> samples;
-        flat_hash_set<Vertex> current_buffer;
+        std::vector<Vertex> current_buffer;
+        std::mt19937_64 mt(seed);
         for(size_t i = 0, nr = std::ceil(std::pow(logn, 1.5)); i < nr; ++i) {
-            sample_from_graph(x, samples_per_round, iterations_per_round, current_buffer);
+            sample_from_graph(x, samples_per_round, iterations_per_round, current_buffer, mt());
             samples.insert(current_buffer.begin(), current_buffer.end());
             current_buffer.clear();
         }
-        return samples;
+        current_buffer.insert(samples.begin(), samples.end());
+        return current_buffer;
     }
-    template<typename G, typename C>
-    auto &sample_from_graph(const G &x, size_t samples_per_round, size_t iterations, C &container) {
+    template<typename G>
+    auto &sample_from_graph(const G &x, size_t samples_per_round, size_t iterations,
+                            std::vector<typename G::Vertex> &container, uint64_t seed) {
         // Algorithm D, Thorup p.415
         using Vertex = typename G::Vertex;
         //using Vertex = graph_traits<Graph>::vertex_descriptor;
         // Let R = all nodes
         auto [start, end] = x.vertices();
-        flat_hash_set<Vertex> R(start, end);
-        flat_hash_set<Vertex> F;
-        wy::WyRand<uint64_t, 2> rng;
-        std::uniform_int_distribution<Vertex> dist;
-        size_t num_el = std::distance(start, end);
+        std::vector<Vertex> R(start, end);
+        auto &F = container;
+        F.reserve(std::min(R.size(), iterations * samples_per_round));
+        wy::WyRand<uint64_t, 2> rng(seed);
+        size_t num_el = R.size();
         // TODO: consider using hash_set distribution for provide randomness for insertion to F.
         // Maybe replace with hash set? Idk.
         for(size_t iter = 0; iter < iterations && R.size() > 0; ++iter) {
             size_t last_size = F.size();
             // Sample ``samples_per_round'' samples.
-            while(F.size() < std::min(num_el, last_size + samples_per_round)) {
-                Vertex ind;
-                do {ind = dist(rng);} while(!F.emplace(ind).second);
+            for(size_t i = 0, e = std::min(samples_per_round, num_el - F.size()); i < e; ++i) {
+                auto &r = R[rng() % R.size()];
+                F.emplace_back(r);
+                std::swap(r, R.back()); // Move to the back
+                R.pop_back();           // Delete
             }
             // Calculate F->R distances
             // For each R, find its nearest neighbor in F
