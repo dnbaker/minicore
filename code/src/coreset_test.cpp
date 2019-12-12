b239feb80cb6779c38643413829473d8ed4c3a81
dnbaker <dnb@cs.jhu.edu>
2019-12-12 16:39:54 -0500

    Test Coreset sampler construction.

--- code/src/coreset_test.cpp
+++ code/src/coreset_test.cpp
@@ -3,5 +3,15 @@
 int main() {
     coresets::Coreset<uint32_t, float> cs(100);
     coresets::CoresetSampler<float, uint32_t> sampler;
-    if(0) sampler.make_sampler(10, 10, nullptr, nullptr);
+    size_t npoints = 10000;
+    std::vector<uint32_t> assignments(npoints);
+    std::vector<float>          costs(npoints);
+    size_t ncenters = 20;
+    for(auto &v: assignments) v = std::rand() % ncenters;
+    for(auto &v: costs)       v = 1. / std::pow(float(std::rand()) / RAND_MAX, 3);
+    sampler.make_sampler(npoints, ncenters, costs.data(), assignments.data() /* weights = nullptr */);
+    std::vector<float> weights(npoints);
+    for(auto &v: weights)       v = 1. / std::pow(float(std::rand()) / RAND_MAX, 2);
+    sampler.make_sampler(npoints, ncenters, costs.data(), assignments.data(), weights.data(), 2);
+    //if(0) sampler.make_sampler(10, 10, nullptr, nullptr);
 }
