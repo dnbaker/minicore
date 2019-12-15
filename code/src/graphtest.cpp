f222d3d20a94f355413c33947950e89f718a9dae
dnbaker <dnb@cs.jhu.edu>
2019-12-14 21:46:38 -0500

    Travis but with boost.

--- code/src/graphtest.cpp
+++ code/src/graphtest.cpp
@@ -13,20 +13,21 @@ auto dimacs_parse(const char *fn) {
     auto g = parse_dimacs_unweighted<boost::undirectedS>(fn);
     using Graph = decltype(g);
     boost::graph_traits<decltype(g)>::edge_iterator ei, ei_end;
-    typedef boost::graph_traits<Graph> GraphTraits;
+    //typedef boost::graph_traits<Graph> GraphTraits;
     typename boost::property_map<Graph, boost::vertex_index_t>::type index = get(boost::vertex_index, g);
     //typename boost::property_map<Graph, boost::edge_weight_t>::type weight = get(boost::edge_weight, g);
     //typename boost::property_map<Graph, boost::edge_weight_t>::type weightMap = 
     //               get(boost::edge_weigh_t, graph);
     property_map<Graph, edge_weight_t>::type weightmap = get(edge_weight, g);
+    std::fprintf(stderr, "address: %p. index: %p\n", (void *)&weightmap, (void *)&index);
     for(std::tie(ei, ei_end) = boost::edges(g); ei != ei_end; ++ei) {
         auto ed = *ei;
         auto src = source(*ei, g);
         auto dest = target(*ei, g);
         auto v = boost::get(boost::edge_weight_t(), g, *ei);
-        std::fprintf(stderr, "[%d] value: %f. s: %d. dest: %d\n", ed, v, index[src], index[dest]);
+        std::fprintf(stderr, "[%p] value: %f. s: %d. dest: %d\n", (void *)&ed, v, unsigned(index[src]), unsigned(index[dest]));
         boost::put(boost::edge_weight_t(), g, *ei, 1.37);
-        std::fprintf(stderr, "after value: %f. s: %d. dest: %d\n", boost::get(boost::edge_weight_t(), g, *ei), index[src], index[dest]);
+        std::fprintf(stderr, "after value: %f. s: %d. dest: %d\n", boost::get(boost::edge_weight_t(), g, *ei), unsigned(index[src]), unsigned(index[dest]));
         //typename GraphTraits::edge_descriptor e;
         //std::fprintf(stderr, "edge weight: %f. edge id: %d\n", g[*ei], (int)*ei);
     }
@@ -37,13 +38,16 @@ auto dimacs_parse(const char *fn) {
         //auto vind = *vs;
         //TD<decltype(vind)> td;
     }
+    size_t edgecount = 0;
     for(const auto &e: g.edges()) {
+        edgecount += reinterpret_cast<uint64_t>(&e) % 65536;
         //std::fprintf(stderr, "WOOO\n");
     }
+    edgecount = 0;
     for(const auto &e: g.vertices()) {
-        (void)e;
-        //std::fprintf(stderr, "WOOO\n");
+        edgecount += reinterpret_cast<uint64_t>(&e) % 65536;
     }
+    std::fprintf(stderr, "sum of hash remainders: %zu\n", edgecount);
     std::vector<typename Graph::Vertex> top;
     try {
         top = g.toposort();
