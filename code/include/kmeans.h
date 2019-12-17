5665c212128d581d96bc8fda5d78977603902a74
dnbaker <dnb@cs.jhu.edu>
2019-12-17 01:06:27 -0500

    Get assignments for kmeans++. TODO: Lloyd's for k-means.

--- code/include/kmeans.h
+++ code/include/kmeans.h
@@ -27,13 +27,12 @@ using ContainedTypeFromIterator = std::decay_t<decltype((*std::declval<C>())[0])
 
 template<typename Iter, typename FT=ContainedTypeFromIterator<Iter>,
          typename IT=std::uint32_t, typename RNG, typename Norm=sqrL2Norm>
-std::pair<std::vector<IT>, std::vector<FT>>
+std::tuple<std::vector<IT>, std::vector<IT>, std::vector<FT>>
 kmeanspp(Iter first, Iter end, RNG &rng, size_t k, const Norm &norm=Norm()) {
     static_assert(std::is_floating_point<FT>::value, "FT must be fp");
     size_t np = end - first;
     std::vector<IT> centers;
     std::vector<FT> distances(np, 0.), cdf(np);
-    //std::vector<IT> assignments(np, IT(-1));
     double sumd2 = 0.;
     {
         auto fc = rng() % np;
@@ -61,10 +60,12 @@ kmeanspp(Iter first, Iter end, RNG &rng, size_t k, const Norm &norm=Norm()) {
     std::fprintf(stderr, "first loop sum: %f. manual: %f\n", sumd2, std::accumulate(distances.begin(), distances.end(), double(0)));
 #endif
         
+    std::vector<IT> assignments(np);
     while(centers.size() < k) {
         // At this point, the cdf has been prepared, and we are ready to sample.
         // add new element
         auto newc = std::lower_bound(cdf.begin(), cdf.end(), cdf.back() * double(rng()) / rng.max()) - cdf.begin();
+        const auto current_center_id = centers.size();
         centers.push_back(newc);
         const auto &lhs = first[newc];
         sumd2 -= distances[newc];
@@ -75,6 +76,7 @@ kmeanspp(Iter first, Iter end, RNG &rng, size_t k, const Norm &norm=Norm()) {
             auto &ldist = distances[i];
             double dist = norm(lhs, first[i]);
             if(dist < ldist) { // Only write if it changed
+                assignments[i] = current_center_id;
                 auto diff = dist - ldist;
                 sum += diff;
                 ldist = dist;
@@ -86,13 +88,13 @@ kmeanspp(Iter first, Iter end, RNG &rng, size_t k, const Norm &norm=Norm()) {
 #endif
         inclusive_scan(distances.begin(), distances.end(), cdf.begin());
     }
-    return std::make_pair(std::move(centers), std::move(distances));
+    return std::make_tuple(std::move(centers), std::move(assignments), std::move(distances));
 }
 template<typename FT, bool SO,
          typename IT=std::uint32_t, typename RNG, typename Norm=sqrL2Norm>
 auto
 kmeanspp(const blaze::DynamicMatrix<FT, SO> &mat, RNG &rng, size_t k, const Norm &norm=Norm(), bool rowwise=true) {
-    std::pair<std::vector<IT>, std::vector<FT>> ret;
+    std::tuple<std::vector<IT>, std::vector<IT>, std::vector<FT>> ret;
     const auto &blzview = reinterpret_cast<const blz::DynamicMatrix<FT, SO> &>(mat);
     if(rowwise) {
         auto rowit = blzview.rowiterator();
@@ -112,11 +114,11 @@ auto kmeans_coreset(Iter start, Iter end,
                     size_t k, RNG &rng,
                     size_t cs_size,
                     const FT *weights=nullptr) {
-    auto [centers, sqdists] = kmeanspp(start, end, rng, k, sqrL2Norm());
+    auto [centers, assignments, sqdists] = kmeanspp(start, end, rng, k, sqrL2Norm());
     using sq_t = typename decltype(sqdists)::value_type;
     coresets::CoresetSampler<sq_t, IT> cs;
     size_t np = end - start;
-    std::vector<IT> assignments(np);
+#if 0
     // Get assignments
     OMP_PRAGMA("parallel for")
     for(size_t i = 0; i < np; ++i) {
@@ -132,6 +134,7 @@ auto kmeans_coreset(Iter start, Iter end,
         sqdists[i] = minv;
         assignments[i] = assign_index;
     }
+#endif
     cs.make_sampler(np, centers.size(), sqdists.data(), assignments.data(), weights,
                     /*seed=*/rng());
     coresets::IndexCoreset<IT, sq_t> ics(cs.sample(cs_size, rng()));
@@ -151,4 +154,4 @@ auto kmeans_matrix_coreset(const blaze::DynamicMatrix<FT, SO> &mat, size_t k, RN
 }
 #undef inclusive_scan
 
-} // clustering
+} // namespace clustering
