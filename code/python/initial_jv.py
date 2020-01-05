2f140f20fa305e60946772a21620553eb0631d3c
dnbaker <dnb@cs.jhu.edu>
2020-01-04 23:47:17 -0500

    Simplify.

--- code/python/initial_jv.py
+++ code/python/initial_jv.py
@@ -8,7 +8,7 @@ false = False
 np.random.seed(13)
 
 nf = 5
-nc = 25
+nc = 200
 
 city_locs = np.power(np.random.standard_cauchy((nc, 2)).astype(np.float64) + np.random.normal(size=(nc,2)), 2)
 f_locs = np.power(np.random.standard_cauchy((nf, 2)).astype(np.float64) + np.random.normal(size=(nf,2)), 2)
@@ -16,7 +16,7 @@ f_locs = np.power(np.random.standard_cauchy((nf, 2)).astype(np.float64) + np.ran
 c = np.array([[np.linalg.norm(f - c) for c in city_locs] for f in f_locs])
 assert np.all(c > 0)
 
-f_costs = np.array([1.] * nf)
+f_costs = np.array([10.] * nf)
 
 print("costs:", c.astype(np.int32))
 print("costs to open facilities: ", f_costs.astype(np.int32))
@@ -32,13 +32,15 @@ S = set(range(nc))
 Sp = []
 
 istight = np.zeros((nf, nc), dtype=np.bool)
-istempopen = np.zeros(nf, dtype=np.bool)
+tos = set()
+ntos = set(range(nf))
 isopen = np.zeros(nf, dtype=np.bool)
 
 edges = [(c[i,j], i, j) for i in range(nf) for j in range(nc)]
-edges.sort(key=lambda x: x[0])
-edge_index = 0
-assert all(edges[i][0] <= edges[i + 1][0] for i in range(nc * nf - 1))
+edges.sort(key=lambda x: -x[0])
+etop = lambda: edges[-1]
+epop = lambda: edges.pop()
+assert all(edges[i][0] >= edges[i + 1][0] for i in range(nc * nf - 1))
 
 
 def get_cost_to_open(*, w, c, fid, v, f_costs,s):
@@ -57,18 +59,17 @@ maxalph = 0.
 
 # Phase 1
 while S:
-    print("S [size: %u] : %s. nfac temp open: %d" % (len(S), S, np.sum(istempopen)))
+    print("S [size: %u] : %s. nfac temp open: %d" % (len(S), S, len(tos)))
     # Get minimum cost to make an edge go tight
     # We go through them in order, so it's easy
-    mincostedge = edges[edge_index]        # edge of minimum cost
+    mincostedge = etop()        # edge of minimum cost
     minedgecost = mincostedge[0] - maxalph # how much to increment to make that edge go tight
     print("mincostedge cost: %f. alpha inc: %f (maxalph: %f)" % (mincostedge[0], minedgecost, maxalph))
 
     # Get minimum cost to open a facility
     minfaccost = np.inf
     minfacind = -1
-    for fid in range(nf):
-        if istempopen[fid]: continue
+    for fid in ntos:
         cost_to_open = get_cost_to_open(w=w, c=c, fid=fid, v=v, f_costs=f_costs,s=S)
         assert cost_to_open >= 0.
         if cost_to_open < minfaccost:
@@ -77,7 +78,7 @@ while S:
             minfacind = fid
         # Cost = cost_fac - sum(willignness to pay for fid)
     if minedgecost < minfaccost:
-        edge_index += 1  # Move to next edge index
+        epop()  # Move to next edge index
         tighten_edge = true
     else:
         tighten_edge = false
@@ -95,7 +96,7 @@ while S:
             if v[s] >= c[fid,s]:
                 istight[fid,s] = true
                 print("s is tight: %d" % s)
-                if istempopen[fid]:
+                if fid in tos:
                     to_remove.add(s)
             # w[i,j] := max(0., v[j] - c[j][i]); in other words, w[j,i] is how much client i 
             oldw = w[fid,s]
@@ -110,8 +111,11 @@ while S:
         assert minfacind >= 0
         print("minfacind: %d. cost to open: %f. sum of contributions: %f" % (minfacind, f_costs[minfacind], np.sum(w[minfacind,:])))
         # This means that we temporarily open a facility instead
-        assert np.sum(f_costs[minfacind] <= np.sum(w[minfacind,:]))
-        istempopen[minfacind] = true
+        contrib_sum = np.sum(w[minfacind,:])
+        fc = f_costs[minfacind]
+        assert fc <= contrib_sum + 1e-10, f"diff: {abs(f_costs[minfacind] - np.sum(w[minfacind,:]))}"
+        tos.add(minfacind)
+        ntos.remove(minfacind)
         print("just opened fac %d" % minfacind)
         for cid in S:
             if v[cid] >= c[minfacind,cid]:  # If willingness to pay exceeds cost
@@ -121,7 +125,6 @@ while S:
         Handle if multiple open at exactly the same time
         for fid in range(nf):
             if np.sum(f_costs[minfacind] <= np.sum(w[minfacind,:])):
-                istempopen
         '''
 
     for item in to_remove:  # Note: this way, we can ultimately only update them when we remove them
@@ -131,20 +134,27 @@ while S:
 
 # Phase 2: electric boogaloo
 
-tempopen = list(np.where(istempopen)[0])
+tempopen = list(tos)
+for fac in tempopen:
+    assert sum(w[fac,:] >= 0.)
 random.shuffle(tempopen)
-lastadded = tempopen[-1]
+lastadded = tempopen.pop()
 
 tprime = {lastadded}
 while 1:
+    print("tempopen: %s" % tempopen)
     contributors = np.where(w[lastadded,:] > 0.)[0]
     to_remove = set()
     for contrib in contributors:
         supporting = np.where(w[:,contrib] > 0.)[0]
         to_remove.update(supporting)
-    tempopen = list(set(tempopen) - to_remove)
+    print("removing: %s" % to_remove)
+    if to_remove:
+        tempopen = list(set(tempopen) - to_remove)
     if not tempopen: break
-    lastadded = random.choice(tempopen)
+    random.shuffle(tempopen)
+    lastadded = tempopen.pop()
+    tprime.add(lastadded)
 
 t = tprime
 print("Set: %s" % t)
