eaea0b996d9b7c16a88050236aa1891afa366a51
dnbaker <dnb@cs.jhu.edu>
2020-01-06 21:08:10 -0500

    JV runs.

--- code/src/jvtest.cpp
+++ code/src/jvtest.cpp
@@ -6,5 +6,28 @@ int main() {
     std::vector<typename fgc::Graph<boost::undirectedS, float>::vertex_descriptor> vxs(g.vertices().begin(), g.vertices().end());
     if(0) {
         fgc::jain_vazirani_kmedian(g, vxs, 15);
-    } else std::fprintf(stderr, "This only checks compilation, not correctness, of JV draft\n");
+    }
+    std::fprintf(stderr, "Getting here only checks compilation, not correctness, of JV draft.\n");
+    size_t dim = 20;
+    size_t np = 1000;
+    blaze::DynamicMatrix<float> points(np, dim);
+    std::mt19937_64 mt;
+    std::uniform_real_distribution<float> urd;
+    for(auto r: blz::rowiterator(points))
+        for(auto &v: r)
+            v = urd(mt);
+    std::set<int> indices;
+    while(indices.size() < 20)
+        indices.insert(std::rand() % points.rows());
+    std::vector<int> iv(indices.begin(), indices.end());
+    blaze::DynamicMatrix<float> facilities = rows(points, iv);
+    assert(facilities.rows() == 20);
+    blaze::DynamicMatrix<float> dists(facilities.rows(), points.rows());
+    for(size_t i = 0; i < facilities.rows(); ++i)
+        for(size_t j = 0; j < points.rows(); ++j)
+            dists(i, j) = blz::l2Dist(row(facilities, i), row(points, j));
+    fgc::NaiveJVSolver<float> njv(dists.rows(), dists.columns());
+    njv.setup(dists);
+    auto res = njv.ufl(dists, 1.5);
+    std::fprintf(stderr, "res size: %zu\n", res.size());
 }
