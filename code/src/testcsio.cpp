71df05545228d9cd97c332f324a04085276b53e5
dnbaker <dnb@cs.jhu.edu>
2020-01-13 15:41:31 -0500

    Missing.

---
+++ code/src/testcsio.cpp
@@ -0,0 +1,17 @@
+#include "fgc/coreset.h"
+
+using FT = float;
+using namespace coresets;
+int main() {
+    CoresetSampler<FT> cs;
+    std::vector<float> costs(100);
+    std::vector<unsigned> asn(100);
+    for(auto &i: asn) i = std::rand() % 5;
+    for(auto &i: costs) i = float(std::rand()) / RAND_MAX;
+    cs.make_sampler(100, 5, costs.data(), asn.data());
+    cs.write("test_io.cs");
+    CoresetSampler<FT> cs2("test_io.cs");
+    assert(cs == cs2);
+    std::system("rm test_io.cs");
+}
+
