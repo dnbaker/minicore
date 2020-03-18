ee5a4de4b525c68e39d6a5da733c12b7d37ab8b6
dnbaker <dnb@cs.jhu.edu>
2020-03-18 11:24:48 -0400

    Composite LSH table.

--- code/include/fgc/hash.h
+++ code/include/fgc/hash.h
@@ -3,10 +3,22 @@
 #define FGC_HASH_H__
 #include "blaze_adaptor.h"
 #include <random>
+#include "xxHash/xxh3.h"
+#include "xxHash/xxhash.h"
+#ifdef _OPENMP
+#  include <mutex>
+#endif
 
 namespace fgc {
 
 namespace hash {
+
+struct LSHasherSettings {
+    unsigned dim_;
+    unsigned k_;
+    unsigned l_;
+    unsigned nhashes() const {return k_ * l_;}
+};
 
 template<typename FT=double, bool SO=blz::rowMajor>
 class JSDLSHasher {
@@ -15,15 +27,18 @@ class JSDLSHasher {
     // Note that this is an LSH for the JS Divergence, not the metric.
     blz::DM<FT, SO> randproj_;
     blz::DV<FT, SO> boffsets_;
+    LSHasherSettings settings_;
 public:
     using ElementType = FT;
     static constexpr bool StorageOrder = SO;
-    JSDLSHasher(size_t nd, size_t nh, double r, uint64_t seed=0) {
+    JSDLSHasher(LSHasherSettings settings, double r, uint64_t seed=0): settings_(settings) {
+        unsigned nd = settings.dim_, nh = settings.nhashes();
         if(seed == 0) seed = nd * nh + r;
         std::mt19937_64 mt(seed);
         std::normal_distribution<FT> gen;
         randproj_ = blz::generate(nh, nd, [&](size_t, size_t){return gen(mt);}) / r;
         boffsets_ = blz::generate(nh, [&](size_t){return FT(mt()) / mt.max();});
+        assert(settings_.k_ * settings_.l_ == randproj_.rows()); // In case of overflow, I suppose
     }
     template<typename VT>
     decltype(auto) hash(const blz::Vector<VT, SO> &input) const {
@@ -45,8 +60,12 @@ public:
         //std::fprintf(stderr, "Reversed SO input rows/col: %zu/%zu. my rows/col:%zu/%zu\n", (~input).rows(), (~input).columns(), randproj_.rows(), randproj_.columns());
         return trans(blz::ceil(randproj_ * ~input + blz::expand(boffsets_, (~input).columns())));
     }
+    const auto &matrix() const {return randproj_;}
     auto dim() const {return randproj_.columns();}
-    auto nh()  const {return boffsets_.size();}
+    auto nh()  const {return settings_.nhashes();}
+    auto k()   const {return settings_.k_;}
+    auto l()   const {return settings_.l_;}
+    const auto &settings() const {return settings_;}
 };
 
 template<typename FT=double, bool SO=blz::rowMajor>
@@ -57,15 +76,20 @@ class S2JSDLSHasher {
     // Note that this is an LSH for the JS Metric, not the JSD.
     blz::DM<FT, SO> randproj_;
     blz::DV<FT, SO> boffsets_;
+    LSHasherSettings settings_;
+    double w_;
 public:
     using ElementType = FT;
     static constexpr bool StorageOrder = SO;
-    S2JSDLSHasher(size_t nd, size_t nh, double w, uint64_t seed=0) {
+    S2JSDLSHasher(LSHasherSettings settings, double w, uint64_t seed=0): settings_(settings), w_(w) {
+        auto nh = settings.nhashes();
+        auto nd = settings.dim_;
         if(seed == 0) seed = nd * nh  + w + 1. / w;
         std::mt19937_64 mt(seed);
         std::normal_distribution<FT> gen;
         randproj_ = blz::abs(blz::generate(nh, nd, [&](size_t, size_t){return gen(mt);}) * (4. / (w * w)));
         boffsets_ = blz::generate(nh, [&](size_t){return FT(mt() / 2) / mt.max();}) - 0.5;
+        assert(settings_.k_ * settings_.l_ == randproj_.rows()); // In case of overflow, I suppose
     }
     template<typename VT>
     decltype(auto) hash(const blz::Vector<VT, SO> &input) const {
@@ -83,39 +107,88 @@ public:
     decltype(auto) hash(const blz::Matrix<MT, !SO> &input) const {
         return trans(blz::floor(blz::sqrt(randproj_ * (trans(~input)) + 1.) + blz::expand(boffsets_, (~input).columns())));
     }
-    auto dim() const {return randproj_.columns();}
-    auto nh()  const {return boffsets_.size();}
+    const auto &matrix() const {return randproj_;}
+    auto dim() const {return settings_.dim_;}
+    auto nh()  const {return settings_.nhashes();}
+    auto k()   const {return settings_.k_;}
+    auto l()   const {return settings_.l_;}
+    const auto &settings() const {return settings_;}
 };
 
-template<typename Hasher, typename IT=::std::uint32_t, typename KT=IT>
+struct XXHasher32 {
+    const uint32_t seed_;
+    using type = uint32_t;
+    XXHasher32(uint32_t seed=0): seed_(seed) {
+    }
+    INLINE uint32_t operator()(const void *dat, size_t nb) const {
+        return XXH32(dat, nb, seed_);
+    }
+};
+struct XXHasher64 {
+    const uint64_t seed_;
+    using type = uint64_t;
+    XXHasher64(uint64_t seed=0): seed_(seed) {
+    }
+    INLINE uint64_t operator()(const void *dat, size_t nb) const {
+        return XXH3_64bits_withSeed(dat, nb, seed_);
+    }
+};
+
+template<typename KT> struct XXHasher:
+    public std::conditional_t<sizeof(KT) == 8, XXHasher64, XXHasher32>
+{
+    using super = std::conditional_t<sizeof(KT) == 8, XXHasher64, XXHasher32>;
+    XXHasher(typename super::type seed): super(seed) {}
+    static_assert(std::is_integral<KT>::value || sizeof(KT) >= 16, "KT must be integral __{u,}int128 aren't guaranteed to have type_traits defined accordingly");
+};
+
+template<typename Hasher, typename IT=::std::uint32_t, typename KT=uint64_t>
 struct LSHTable {
     using ElementType = typename Hasher::ElementType;
     const Hasher hasher_;
     std::unique_ptr<shared::flat_hash_map<KT, std::vector<IT>>[]> tables_;
     const unsigned nh_;
+    XXHasher<KT> xxhasher_;
+    OMP_ONLY(std::unique_ptr<std::mutex[]> mutexes;)
+
     static constexpr bool SO = Hasher::StorageOrder;
 
 private:
     INLINE void insert(unsigned i, KT key, IT id) {
         auto &table = tables_[i];
-        auto it = table.find(key);
-        if(it == table.end()) it = table.emplace(key, std::vector<IT>{id}).first;
-        else                  it->second.push_back(id);
+        {
+            OMP_ONLY(std::lock_guard<std::mutex> lock(mutexes[i]);)
+            auto it = table.find(key);
+            if(it == table.end()) it = table.emplace(key, std::vector<IT>{id}).first;
+            else                  it->second.push_back(id);
+        }
     }
 public:
 
     template<typename...Args>
     LSHTable(Args &&...args): hasher_(std::forward<Args>(args)...),
-                              tables_(new shared::flat_hash_map<KT, std::vector<IT>>[hasher_.nh()]),
-                              nh_(hasher_.nh())
+                              tables_(new shared::flat_hash_map<KT, std::vector<IT>>[hasher_.l()]),
+                              nh_(hasher_.nh()),
+                              xxhasher_(XXH3_64bits_withSeed(hasher_.matrix().data(), hasher_.matrix().spacing() *
+                                  (blaze::IsRowMajorMatrix_v<
+                                       std::decay_t<decltype(hasher_.matrix())>
+                                   > ? hasher_.matrix().rows(): hasher_.matrix().columns()),
+                                  hasher_.nh()))
+                              OMP_ONLY(, mutexes(new std::mutex[hasher_.l()]))
     {
     }
+    LSHTable(const LSHTable &) = delete;
+    LSHTable(LSHTable &&)     = default;
+
     void sort() {
         OMP_PRAGMA("omp parallel for schedule(dynamic)")
         for(unsigned i = 0; i < tables_.size(); ++i)
             for(auto &pair: tables_[i])
                 shared::sort(pair.second.begin(), pair.second.end());
     }
+    const LSHasherSettings &settings() const {return hasher_.settings();}
+    auto k()   const {return settings().k_;}
+    auto l()   const {return settings().l_;}
     template<typename Query>
     decltype(auto) hash(const Query &q) const {
         return hasher_.hash(q);
@@ -123,35 +196,34 @@ public:
     template<typename VT, bool OSO>
     void add(const blz::Vector<VT, OSO> &input, IT id) {
         auto hv = blz::evaluate(hash(input));
-        if(nh_ != hv.size()) {
+        if(unlikely(nh_ != hv.size())) {
             std::fprintf(stderr, "[%s] nh_: %u. hv.size: %zu\n", __PRETTY_FUNCTION__, nh_, hv.size());
             std::exit(1);
         }
-        for(unsigned i = 0; i < nh_; ++i) {
-            insert(i, hv[i], id);
+        auto st = settings();
+        for(size_t i = 0; i < st.l_; ++i) {
+            auto hh = xxhasher_(&hv[i * st.k_], sizeof(ElementType) * st.k_);
+            insert(i, hh, id);
         }
     }
     template<typename MT, bool OSO>
     void add(const blz::Matrix<MT, OSO> &input, IT idoffset=0) {
-        auto hv = blz::evaluate(trans(hash(input)));
+        auto hv = blz::evaluate(hash(input));
         std::fprintf(stderr, "hv shape: %zu/%zu.\n", hv.rows(), hv.columns());
-        if(nh_ != hv.rows()) {
-            std::fprintf(stderr, "[%s] nh_: %u. hv.rows: %zu\n", __PRETTY_FUNCTION__, nh_, hv.rows());
+        if(nh_ != hv.columns()) {
+            std::fprintf(stderr, "[%s] nh_: %u. hv.columns: %zu\n", __PRETTY_FUNCTION__, nh_, hv.columns());
             std::exit(1);
         }
-        if((~input).rows() != hv.columns()) {
-            std::fprintf(stderr, "[%s] nh_: %u. hv.columns: %zu. input rows: %zu\n", __PRETTY_FUNCTION__, nh_, hv.columns(), (~input).rows());
+        if((~input).rows() != hv.rows()) {
+            std::fprintf(stderr, "[%s] nh_: %u. hv.columns: %zu. input rows: %zu\n", __PRETTY_FUNCTION__, nh_, hv.rows(), (~input).rows());
             std::exit(1);
         }
-        // Each thread adds to its own hash table to avoid any contention.
-        OMP_PFOR
-        for(unsigned i = 0; i < nh_; ++i) {
-            auto r = row(hv, i BLAZE_CHECK_DEBUG);
-            assert(r.size() == (~input).rows());
-            SK_UNROLL_4
-            for(IT roff = 0, eoff = r.size(); roff < eoff; ++roff) {
-                std::fprintf(stderr, "hash table %u is getting key %u at index %u\n", i, unsigned(r[roff]), idoffset + roff);
-                insert(i, r[roff], idoffset + roff);
+        const size_t nr = (~input).rows();
+        const auto _l = l(), _k = k();
+        for(unsigned i = 0; i < nr; ++i) {
+            auto r = row(hv, i, blz::unchecked);
+            for(unsigned j = 0; j < _l; ++j) {
+                insert(j, xxhasher_(&r[j * _k], sizeof(ElementType) * _k), idoffset + i);
             }
         }
     }
@@ -159,9 +231,10 @@ public:
     shared::flat_hash_map<IT, unsigned> query(const blz::Vector<VT, OSO> &query) const {
         auto hv = evaluate(hash(query));
         shared::flat_hash_map<IT, unsigned> ret;
-        for(unsigned i = 0; i < nh_; ++i) {
-            auto it = tables_[i].find(hv[i]);
-            if(it != tables_[i].end()) {
+        for(unsigned i = 0; i < l(); ++i) {
+            if(auto it = tables_[i].find(xxhasher_(&hv[i * k()], sizeof(ElementType) * k()));
+               it != tables_[i].end())
+            {
                 for(const auto v: it->second) {
                     auto nit = ret.find(v);
                     if(nit != ret.end()) ++nit->second;
@@ -177,17 +250,17 @@ public:
         auto hv = evaluate(hash(query));
         //std::fprintf(stderr, "hv rows: %zu. columns: %zu. nh: %u. input num rows: %zu. input col: %zu\n", hv.rows(), hv.columns(), nh_, (~query).rows(), (~query).columns());
         if(hv.columns() != nh_) throw std::runtime_error("Wrong number of columns");
-        if(hv.rows() != (~query).rows()) throw std::runtime_error("WRong number of rows");
+        if(hv.rows() != (~query).rows()) throw std::runtime_error("Wrong number of rows");
         std::vector<shared::flat_hash_map<IT, unsigned>> ret(hv.rows());
         assert(hv.rows() == (~query).rows());
         OMP_PFOR
         for(unsigned j = 0; j < hv.rows(); ++j) {
             auto &map = ret[j];
-            typename shared::flat_hash_map<IT, std::vector<IT>>::const_iterator it;
+            typename shared::flat_hash_map<KT, std::vector<IT>>::const_iterator it;
             auto hr = row(hv, j BLAZE_CHECK_DEBUG);
             assert(hr.size() == nh_);
-            for(unsigned i = 0; i < nh_; ++i) {
-                if((it = tables_[i].find(KT(hr[i]))) != tables_[i].cend()) {
+            for(unsigned i = 0; i < l(); ++i) {
+                if((it = tables_[i].find(xxhasher_(&hr[i * k()], sizeof(ElementType) * k()))) != tables_[i].cend()) {
                     for(const auto v: it->second) {
                         auto nit = map.find(v);
                         if(nit != map.end()) ++nit->second;
--- code/src/jsdhash.cpp
+++ code/src/jsdhash.cpp
@@ -3,7 +3,7 @@
 using namespace fgc;
 
 int main() {
-    unsigned dim = 20, nhashes = 4, nsamp = 10;
+    unsigned dim = 200, k = 4, l = 5, nsamp = 10, nhashes = k * l;
     std::mt19937_64 mt(dim  * nhashes);
     std::normal_distribution<float> gen;
     blz::DM<float> dm = blz::generate(nsamp, dim, [&](auto, auto) {return std::abs(gen(mt));});
@@ -11,11 +11,12 @@ int main() {
     assert(dm.columns() == dim);
     std::cout << dm << '\n';
     for(auto r: rowiterator(dm)) r /= blz::sum(r);
-    for(double r = 0.025; r <= 0.025; r *= 10) {
+    hash::LSHasherSettings settings{dim, k, l};
+    for(double r = 0.0025; r <= 0.1; r *= 10) {
         std::cout << "R: " << r << '\n';
-        JSDLSHasher<float> jsdhasher(dim, nhashes, r);
-        S2JSDLSHasher<float> s2hasher(dim, nhashes, r);
-        LSHTable<S2JSDLSHasher<float>> s2table(dim, nhashes, r);
+        JSDLSHasher<float> jsdhasher(settings, r);
+        S2JSDLSHasher<float> s2hasher(settings, r);
+        LSHTable<S2JSDLSHasher<float>> s2table(settings, r);
         std::fprintf(stderr, "made entries\n");
         auto hashedmat = s2table.hash(dm);
         std::fprintf(stderr, "hashed s2\n");
@@ -27,9 +28,6 @@ int main() {
         for(unsigned i = 0; i < q.size(); ++i) {
             for(const auto &pair: q[i]) {
                 std::fprintf(stderr, "query item %u matched reference id %u a total of %u times\n", i, pair.first, pair.second);
-#ifndef NDEBUG
-                if(pair.first == i) assert(pair.second == nhashes);
-#endif
             }
         }
 #if 0
