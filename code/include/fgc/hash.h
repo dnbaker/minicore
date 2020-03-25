eb8cad3a8fee34b7329e6dc09d0ba8df50b7cb73
dnbaker <dnb@cs.jhu.edu>
2020-03-25 11:49:16 -0400

    Hellinger, L1, L2, and TVD LSH families.

--- code/include/fgc/hash.h
+++ code/include/fgc/hash.h
@@ -20,11 +20,33 @@ struct LSHasherSettings {
     unsigned nhashes() const {return k_ * l_;}
 };
 
+template<typename FT>
+struct clipped_cauchy_distribution {
+    const FT max_;
+    mutable std::cauchy_distribution<FT> dist_;
+    FT min() const {return -max_;}
+    FT max() const {return max_;}
+    clipped_cauchy_distribution(FT max=1000.): max_(std::abs(max))
+    {
+    }
+    template<typename RNG>
+    FT operator()(RNG &x) const {
+        FT ret = dist_(x);
+        if(ret > max_) ret = max_;
+        if(ret < -max_) ret = -max_;
+        return ret;
+    }
+    void reset() {}
+};
+
 template<typename FT=double, bool SO=blz::rowMajor>
 class JSDLSHasher {
     // See https://papers.nips.cc/paper/9195-locality-sensitive-hashing-for-f-divergences-mutual-information-loss-and-beyond.pdf
     // for the function.
     // Note that this is an LSH for the JS Divergence, not the metric.
+    //
+    // This relies on a U/H-approximation of the JSD (capacitory discrimination in Topsoe, 2000)
+    // by the Hellinger distance, and uses an LSH for the Hellinger as-is.
     blz::DM<FT, SO> randproj_;
     blz::DV<FT, SO> boffsets_;
     LSHasherSettings settings_;
@@ -53,12 +75,12 @@ public:
     template<typename VT>
     decltype(auto) hash(const blz::Matrix<VT, SO> &input) const {
         //std::fprintf(stderr, "Regular input rows/col: %zu/%zu. my rows/col:%zu/%zu\n", (~input).rows(), (~input).columns(), randproj_.rows(), randproj_.columns());
-        return trans(blz::ceil(randproj_ * trans(~input) + blz::expand(boffsets_, (~input).rows())));
+        return trans(blz::ceil(randproj_ * trans(blz::sqrt(~input)) + blz::expand(boffsets_, (~input).rows())));
     }
     template<typename VT>
     decltype(auto) hash(const blz::Matrix<VT, !SO> &input) const {
         //std::fprintf(stderr, "Reversed SO input rows/col: %zu/%zu. my rows/col:%zu/%zu\n", (~input).rows(), (~input).columns(), randproj_.rows(), randproj_.columns());
-        return trans(blz::ceil(randproj_ * ~input + blz::expand(boffsets_, (~input).columns())));
+        return trans(blz::ceil(randproj_ * blz::sqrt(~input) + blz::expand(boffsets_, (~input).columns())));
     }
     const auto &matrix() const {return randproj_;}
     auto dim() const {return randproj_.columns();}
@@ -67,6 +89,109 @@ public:
     auto l()   const {return settings_.l_;}
     const auto &settings() const {return settings_;}
 };
+template<typename FT=double, bool SO=blz::rowMajor>
+class HellingerLSHasher: public JSDLSHasher<FT, SO> {
+public:
+    template<typename...Args>
+    HellingerLSHasher(Args &&...args): JSDLSHasher<FT, SO>(std::forward<Args>(args)...) {}
+};
+
+
+template<template<typename...> class Distribution, typename FT, bool SO, bool use_offsets, typename...Args>
+class PStableLSHasher {
+    // See S2JSD-LSH: A Locality-Sensitive Hashing Schema for Probability Distributions
+    // https://aaai.org/ocs/index.php/AAAI/AAAI17/paper/view/14692
+    // for the derivation
+    // Note that this is an LSH for the JS Metric, not the JSD.
+    blz::DM<FT, SO> randproj_;
+    blz::DV<FT, SO> boffsets_;
+    LSHasherSettings settings_;
+    double w_;
+public:
+    using ElementType = FT;
+    static constexpr bool StorageOrder = SO;
+    template<typename...CArgs>
+    PStableLSHasher(LSHasherSettings settings, double w, uint64_t seed, CArgs &&...args):
+        settings_(settings), w_(w)
+    {
+        Distribution<FT, Args...> gen(std::forward<CArgs>(args)...);
+        auto nh = settings.nhashes();
+        auto nd = settings.dim_;
+        if(seed == 0) seed = nd * nh  + w + 1. / w;
+        std::mt19937_64 mt(seed);
+        randproj_ = blz::abs(blz::generate(nh, nd, [&](size_t, size_t){return gen(mt);}) * (1. / w));
+        if constexpr(use_offsets)
+            boffsets_ = blz::generate(nh, [&](size_t){return FT(mt() / 2) / mt.max();}) - 0.5;
+        assert(settings_.k_ * settings_.l_ == randproj_.rows()); // In case of overflow, I suppose
+    }
+    template<typename VT>
+    decltype(auto) hash(const blz::Vector<VT, SO> &input) const {
+        if constexpr(use_offsets) return blz::floor(randproj_ * (~input) + 1.) + boffsets_;
+        else                      return blz::floor(randproj_ * (~input));
+    }
+    template<typename VT>
+    decltype(auto) hash(const blz::Vector<VT, !SO> &input) const {
+        if constexpr(use_offsets) return blz::floor(randproj_ * trans(~input) + 1.) + boffsets_;
+        else                      return blz::floor(randproj_ * trans(~input));
+    }
+    template<typename MT>
+    decltype(auto) hash(const blz::Matrix<MT, SO> &input) const {
+        if constexpr(use_offsets)
+            return trans(blz::floor(randproj_ * trans(~input)) + blz::expand(boffsets_, (~input).rows()));
+        else                 
+            return trans(blz::floor(randproj_ * trans(~input)));
+    }
+    template<typename MT>
+    decltype(auto) hash(const blz::Matrix<MT, !SO> &input) const {
+        if constexpr(use_offsets)
+            return trans(blz::floor(randproj_ * trans(~input)) + blz::expand(boffsets_, (~input).columns()));
+        else                 
+            return trans(blz::floor(randproj_ * trans(~input)));
+    }
+    const auto &matrix() const {return randproj_;}
+    auto dim() const {return settings_.dim_;}
+    auto nh()  const {return settings_.nhashes();}
+    auto k()   const {return settings_.k_;}
+    auto l()   const {return settings_.l_;}
+    const auto &settings() const {return settings_;}
+};
+
+template<typename FT=double, bool SO=blz::rowMajor, bool use_offsets=true>
+class L2LSHasher: public PStableLSHasher<std::normal_distribution, FT, SO, use_offsets> {
+public:
+    using super = PStableLSHasher<std::normal_distribution, FT, SO, use_offsets>;
+    template<typename...Args>
+    L2LSHasher(LSHasherSettings settings, double w, uint64_t seed=0, Args &&...args): super(settings, w, seed, std::forward<Args>(args)...)
+    {
+    }
+};
+
+template<typename FT=double, bool SO=blz::rowMajor, bool use_offsets=true>
+class L1LSHasher: public PStableLSHasher<std::cauchy_distribution, FT, SO, use_offsets> {
+public:
+    using super = PStableLSHasher<std::cauchy_distribution, FT, SO, use_offsets>;
+    template<typename...Args>
+    L1LSHasher(LSHasherSettings settings, double w, uint64_t seed=0, Args &&...args): super(settings, w, seed, std::forward<Args>(args)...)
+    {
+    }
+};
+
+template<typename FT=double, bool SO=blz::rowMajor, bool use_offsets=true>
+class ClippedL1LSHasher: public PStableLSHasher<clipped_cauchy_distribution, FT, SO, use_offsets> {
+public:
+    using super = PStableLSHasher<clipped_cauchy_distribution, FT, SO, use_offsets>;
+    template<typename...Args>
+    ClippedL1LSHasher(Args &&...args): super(std::forward<Args>(args)...) {}
+};
+
+template<typename FT=double, bool SO=blz::rowMajor, bool use_offsets=true>
+class TVDLSHasher: public L1LSHasher<FT, SO, use_offsets> {
+public:
+    using super = L1LSHasher<FT, SO, use_offsets>;
+    template<typename...Args>
+    TVDLSHasher(Args &&...args): super(std::forward<Args>(args)...) {}
+};
+
 
 template<typename FT=double, bool SO=blz::rowMajor>
 class S2JSDLSHasher {
@@ -276,8 +401,20 @@ public:
 
 } // namespace hash
 
-using hash::JSDLSHasher;
-using hash::S2JSDLSHasher;
+using hash::L1LSHasher;        // D_{L1} = \sqrt{\sum_{d=1}^D |P_i - Q_i|}
+using hash::L2LSHasher;        // D_{L2} = \sqrt{\sum_{d=1}^D (P_i - Q_i)^2}
+using hash::ClippedL1LSHasher;
+/* Note:
+ *      ClippedL1LSHasher has the same properties as L1, though it is likely to have reduced variance
+        and may be more accurate.
+        It can be parameterized, but by default clips values to be < 1000 or > -1000.
+ */
+
+using hash::JSDLSHasher;       // D_{JS}(P || Q)
+using hash::HellingerLSHasher; // D_{Hellinger}(P || Q) = D_{\ell_2}(\sqrt{P}, \sqrt{Q})
+using hash::S2JSDLSHasher;     // D_{JSM}(P || Q) = \sqrt{D_{JS}(P || Q)}
+using hash::TVDLSHasher;       // D_{TV}(P || Q)  = \frac{D_{\ell_1}(P || Q)}{2}
+
 using hash::LSHTable;
 
 }
