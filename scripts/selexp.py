ffb0287c185319778632308e276b1c72e76f0ecd
dnbaker <dnb@cs.jhu.edu>
2020-06-17 13:55:43 -0400

    Usage, wrapper script, better generalization

---
+++ scripts/selexp.py
@@ -0,0 +1,167 @@
+import sys
+
+DISTS = {
+    'L1': '1', 'L2': '2', 'SQRL2': 'S', 'MKL': 'M',
+    'JSM': 'J', 'JSD': 'j', 'PSL2': 'P',
+    'PL2': 'Q', 'TVD': 'T', 'HELLINGER': 'H',
+    'BHATTACHARYYA_DISTANCE': 'Y', 'BHATTACHARYYA_METRIC': 'b',
+    'COSINE_DISTANCE': 'U',
+    'PROBABILITY_COSINE_DISTANCE': 'u',
+    'REVERSE_MKL': 'R',
+    'ITAKURA_SAITO': 'i'
+}
+
+MEASURES = list(DISTS.keys())
+
+# [Greedy, D2, D2 with one round, cluster]
+CMDZIP = ["greedy", "d2", "d2p1", "cluster"]
+CMDDICT = {k: [True, True, False, False] for k in DISTS.keys()}
+PRIORDICT = {k: False for k in DISTS.keys()}
+DEFAULT_SAMPLE_TRIES = 10
+VANILLA_METRICS =  ('TVD', 'L1', 'L2', 'PL2', 'PSL2', 'SQRL2')
+for k in VANILLA_METRICS:
+    CMDDICT[k][2] = CMDDICT[k][3] = True
+
+for k in ("JSM", "JSD", "HELLINGER", "MKL", "REVERSE_MKL", "ITAKURA_SAITO"):
+    PRIORDICT[k] = [1e-4, 1e-2, 1]
+    
+
+PRIORS = {"DIRICHLET", "NONE", "GAMMA_BETA"}
+COMMAND = {"GREEDY": " -G ", "D2": " -l ", "CLUSTER": ""}
+SENSES = {"FL": " -F ", "VX": " -V ", "BFL": "", "LBK": ' -E '}
+
+
+class Run:
+    def __init__(self, k=10, *, cmd, key, gammabeta=1., sample_tries=DEFAULT_SAMPLE_TRIES, lloyd_iter=100, seed=1337, sm="BFL", threads=None, dest=None, path=None, prior="NONE", r=None):
+        assert key in DISTS, "key must be in %s" % (','.join(DISTS.keys()))
+        assert prior in PRIORS, f"{prior} not in {','.join(PRIORS)}"
+        assert cmd in COMMAND, f"{cmd} not in {','.join(COMMAND)}"
+        assert sm in SENSES, f"{sm} not in {','.join(SENSES)}"
+        self.k = k
+        self.key = key
+        self.path = path
+        self.dest = dest
+        self.prior = prior
+        self.sample_tries = sample_tries
+        self.lloyd_iter = lloyd_iter
+        self.cmd = cmd
+        self.seed = seed
+        self.sm = sm
+        self.gammabeta = gammabeta
+        if self.prior == 'DIRICHLET':
+            self.gammabeta = 1.
+        self.threads = threads
+        self.r = r
+
+
+    # Pass a range to r (list, etc.) to perform multiple times.
+
+    def call(self, path=None, dest=None, r=None, seed=0):
+        from subprocess import Popen, PIPE
+        if not path:
+            path = self.path
+            if not path: raise Exception("")
+        if not dest:
+            dest = self.dest
+            if not dest:
+                dest = path
+        if not r and self.r:
+            r = self.r
+        if r:
+            return list(map(
+                lambda x: self.call(path, f"{dest}.{x}", seed=seed+x), r))
+        if not seed:
+            seed = self.seed
+        if not dest:
+            dest = self.dest
+            if not dest:
+                dest = path
+        cmd = f"mtx2cs {COMMAND[self.cmd]} -{DISTS[self.key]} {SENSES[self.sm]}"
+        cmd += f" -L{self.lloyd_iter} -k{self.k} "
+        if self.prior != 'NONE':
+            cmd = cmd + f" -g{self.gammabeta} "
+        if self.threads:
+            cmd += f" -p{int(self.threads)} "
+        cmd += f" -s{seed} "
+        cmd = f"{cmd} {path} {dest}"
+        #print("About to call '%s'" % cmd)
+        #print("dest", dest, "path", path)
+        p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+        return tuple(p.communicate())
+
+
+
+if __name__ == "__main__":
+    import argparse
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-k", type=int, default=2000)
+    ap.add_argument("-s", "--seed", type=int, default=0)
+    ap.add_argument("path")
+    ap.add_argument("--times", "-t", type=int, default=0)
+    ap.add_argument("--dest", default=None)
+    ap.add_argument("--lloyd-iter", type=int, default=100)
+    ap.add_argument("--logfile", "-l", type=str)
+    args = ap.parse_args()
+    k = args.k
+    path = args.path
+    dest = args.dest if args.dest else path
+    li = args.lloyd_iter
+    ret = []
+    rng = None if not args.times else range(args.times)
+    lf = args.logfile
+    if not lf:
+        if dest:
+            lf = dest
+        else:
+            from functools import reduce
+            lf = reduce(lambda x, y: x ^ hash(y), sys.argv, 0)
+            lf = f"logfile{reduce(lambda x, y: x ^ hash(y), sys.argv, 0)}"
+            print("No logfile provided, using '%s'" % lf, file=sys.stderr)
+    for m in MEASURES:
+        print(f"processing {m}", file=sys.stderr)
+        cmds = CMDDICT[m]
+        mydest = dest + "_" + m
+        # print("dest is %s and mydest is %s" % (dest, mydest))
+        s = PRIORDICT[m]
+        def perform_run(with_cmd):
+            ret = []
+            if s:
+                for g in s:
+                    r = Run(cmd=with_cmd, key=m, k=k, path=path, dest=mydest, gammabeta=g, seed=args.seed, prior="GAMMA_BETA", lloyd_iter=li)
+                    tups = r.call(path=path, dest=mydest, r=rng)
+                    for x, tup in zip(rng, tups):
+                        #print(tup, len(tup))
+                        with open(mydest + f"{g}.{x}.log", "wb") as f:
+                            f.write(tup[0] + tup[1])
+                    ret += tups
+            else:
+                r = Run(cmd=with_cmd, key=m, k=k, seed=args.seed, lloyd_iter=li)
+                tups = r.call(path=path, dest=mydest, r=rng)
+                for x, tup in zip(rng, tups):
+                    with open(mydest + f"{x}.log", "wb") as f:
+                        f.write(tup[0] + tup[1])
+                ret += tups
+            return ret
+        if cmds[0]:
+            print("Doing Greedy", file=sys.stderr)
+            ret += perform_run("GREEDY")
+        if cmds[1]:
+            print("Doing D2", file=sys.stderr)
+            ret += perform_run("D2")
+        if cmds[3]:
+            print("Doing CLUSTER", file=sys.stderr)
+            ret += perform_run("CLUSTER")
+        if cmds[2]:
+            tmpli = li
+            li = 1
+            ret += perform_run("CLUSTER")
+            li = tmpli
+    ulog = lf + ".complete.log"
+    olog = lf + ".out.log"
+    elog = lf + ".err.log"
+    ufp, ofp, efp = map(lambda x: open(x, "w"), (ulog, olog, elog))
+    for out, err in map(lambda x: (x[0].decode(), x[1].decode()), ret):
+        ufp.write(out + err)
+        ofp.write(out)
+        efp.write(err)
+    sys.exit(0)
--- src/mtx2cs.cpp
+++ src/mtx2cs.cpp
@@ -18,6 +18,7 @@ void usage() {
                          "-f: Use floats (instead of doubles)\n"
                          "-p: Set number of threads [1]\n"
                          "-x: Transpose matrix (to swap feature/instance labels) during loading.\n"
+                         "-s: Set random seed\n"
                          "-C: load csr format (4 files) rather than matrix.mtx\n\n\n"
                          "=== Dissimilarity Measures ===\n"
                          "-1: Use L1 Norm \n"
@@ -28,6 +29,7 @@ void usage() {
                          "-j: Use multinomial Jensen-Shannon divergence\n"
                          "-J: Use multinomial Jensen-Shannon metric (square root of JSD)\n"
                          "-P: Use probability squared L2 norm\n"
+                         "-Q: Use probability L2 norm\n"
                          "-T: Use total variation distance\n"
                          "-i: Use Itakura-Saito distance\n"
                          "-I: Use reverse Itakura-Saito distance\n"
@@ -300,15 +302,17 @@ enum ResultType {
 
 int main(int argc, char **argv) {
     opts.stamper_.reset(new util::TimeStamper("argparse"));
+    std::fprintf(stderr, "[CLI: '");
+    for(char **av; *av; std::fprintf(stderr, "%s ", *av++));
+    std::fprintf("']\n");
     std::string inpath, outpath;
     [[maybe_unused]] bool use_double = true;
     ResultType rt = ResultType::CORESET;
     for(int c;(c = getopt(argc, argv, "s:c:k:g:p:K:L:uURlGHiIYQbFVPBdjJxSMT12NCDfh?")) >= 0;) {
         switch(c) {
-            case 'R': opts.dis = dist::REVERSE_MKL; break;
             case 'p': OMP_ONLY(omp_set_num_threads(std::atoi(optarg));) break;
             case 'h': case '?': usage();          break;
-            case 'c': opts.coreset_samples = std::strtoull(optarg, nullptr, 10); break;
+
             case '1': opts.dis = dist::L1;        break;
             case '2': opts.dis = dist::L2;        break;
             case 'H': opts.dis = dist::HELLINGER; break;
@@ -325,22 +329,34 @@ int main(int argc, char **argv) {
             case 'j': opts.dis = dist::JSD;       break;
             case 'u': opts.dis = dist::PROBABILITY_COSINE_DISTANCE; break;
             case 'U': opts.dis = dist::COSINE_DISTANCE; break;
+            case 'R': opts.dis = dist::REVERSE_MKL; break;
+
+            case 'F': opts.sm = coresets::FL; break;
+            case 'V': opts.sm = coresets::VARADARAJAN_XIAO; break;
+            case 'E': opts.sm = coresets::LBK; break;
+
             case 'G': rt = ResultType::GREEDY_SELECTION; break;
-            case 'D': opts.discrete_metric_search = true; break;
+			case 'l': rt = ResultType::D2_SAMPLING; break;
+
             case 'g': opts.gamma = std::atof(optarg); opts.prior = dist::GAMMA_BETA; break;
+            case 'd': opts.prior = dist::DIRICHLET; break;
+
+            case 'c': opts.coreset_samples = std::strtoull(optarg, nullptr, 10); break;
+            case 'D': opts.discrete_metric_search = true; break;
+
             case 'k': opts.k = std::atoi(optarg); break;
             case 'K': opts.kmc2_rounds = std::strtoull(optarg, nullptr, 10); break;
             case 'L': opts.lloyd_max_rounds = std::strtoull(optarg, nullptr, 10); break;
             case 'B': opts.load_blaze = true; opts.load_csr = false; break;
             case 'C': opts.load_csr = true;       break;
-            case 'd': opts.prior = dist::DIRICHLET; break;
-			case 'l': rt = ResultType::D2_SAMPLING; break;
             case 's': opts.seed = std::strtoull(optarg,0,10); break;
-            case 'F': opts.sm = coresets::FL; break;
-            case 'V': opts.sm = coresets::VARADARAJAN_XIAO; break;
             case 'x': opts.transpose_data = true; break;
             case 'f': use_double = false;         break;
         }
+    }
+    if(dist::detail::is_bregman(opts.dis) && opts.sm != coresets::LBK && rt == CORESET) {
+        std::fprintf(stderr, "Bregman divergences need LBK coreset construction. Switching to it from %s\n", coresets::sm2str(opts.sm));
+        opts.sm = coresets::LBK;
     }
     if(argc == optind) usage();
     if(argc - 1 >= optind) {
