2d4b01a6ccd04592d4089615017454a98d95cbbb
dnbaker <dnb@cs.jhu.edu>
2020-06-30 13:01:44 -0400

    blz2dims

--- src/utils/blz2dims.cpp
+++ src/utils/blz2dims.cpp
@@ -5,7 +5,23 @@
 
 int main(int, char **argv) {
     blaze::Archive<std::ifstream> arch(argv[1]);
-    blaze::CompressedMatrix<double> mat;
-    arch >> mat;
-    std::cerr << mat.rows() << ", " << mat.columns() << '\n';
+#define TRYTYPE(type) do {\
+    try {\
+    blaze::CompressedMatrix<double> mat;\
+    arch >> mat;\
+    std::cerr << mat.rows() << ", " << mat.columns() << '\n';\
+    return 0;\
+    } catch(...) {}\
+    } while(0);
+    TRYTYPE(double);
+    TRYTYPE(float);
+    TRYTYPE(uint8_t);
+    TRYTYPE(uint16_t);
+    TRYTYPE(uint32_t);
+    TRYTYPE(uint64_t);
+    TRYTYPE(int8_t);
+    TRYTYPE(int16_t);
+    TRYTYPE(int32_t);
+    TRYTYPE(int64_t);
+    throw std::runtime_error("Unrecognized type");
 }
