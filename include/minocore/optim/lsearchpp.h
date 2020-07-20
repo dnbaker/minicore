18eb06cca7b649165bb8703008ec674b1f140418
dnbaker <dnb@cs.jhu.edu>
2020-07-20 08:50:17 -0400

    minor cleanup

--- include/minocore/optim/lsearchpp.h
+++ include/minocore/optim/lsearchpp.h
@@ -115,52 +115,11 @@ auto localsearchpp_rounds(const Oracle &oracle, RNG &rng, DistC &distances, Dist
     return total_gain;
 }
 
-#if 0
-template<typename Oracle, typename RNG, typename DistC, typename CtrsC, typename AsnC, typename WFT=double>
-void localsearchpppp_rounds(const Oracle &oracle, RNG &rng, DistC &distances, DistC &cdf, CtrsC &ctrs, typename AsnC, size_t np, int k, size_t nrounds, int batchsize=3, const WFT *weights=nullptr) {
-    using value_type = std::decay_t<decltype(*std::begin(distances))>;
-    //blz::DV<value_type> newdists = blaze::CustomVector<value_type, blaze::unaligned, blaze::unpadded>(distances.data(), distances.size());
-    std::uniform_real_distribution<value_type> dist;
-    auto make_sel = [&]() -> std::ptrdiff_t {return std::lower_bound(cdf.begin(), cdf.end(), dist(rng) * cdf.back()) - cdf.begin();};
-    blz::DM<value_type> ctrcostmat = blaze::generate(np, ctrs.size(), [&](auto x, auto y) {
-        return oracle(ctrs[x], y);
-    });
+} // namespace coresets
 
-    blz::DV<value_type> ctrcosts(ctrs.size());
-    blaze::DM<value_type> newcosts(batchsize, np), ncv(np);
-    value_type gain = 0.;
-    value_type total_gain = 0.;
-    std::vector<unsigned> idx(batchsize);
-    for(size_t major_round = 0; major_round < nrounds; ++major_round) {
-        for(auto &i: idx) i = make_sel();
-        newcosts = blaze::generate(batchsize, np, [&](auto x, auto y) {return oracle(idx[x], y);});
-        ncv = blaze::min<rowwise>(newcosts);
-        ctrcosts = 0.;
-        for(size_t i = 0; i < np; ++i) {
-            if(ncv[i] >= distances[i]) {
-                ctrcosts += row(ctrcostmat, i) - newcv;
-            } else {
-                gain += distances[i] - newcosts[i];
-            }
-        }
-        auto argmin = std::min_element(ctrcosts.begin(), ctrcosts.end(),) - ctrcosts.begin();
-        auto delta = ctrcosts[i] - gain;
-        if(delta < 0.) {
-            std::fprintf(stderr, "Swapping out %zd for %zd for a gain of %g\n", ctrs[argmin], sel, delta);
-            ctrs[argmin] == sel;
-            row(ctrcostmat, argmin, blaze::unchecked) = newcosts;
-            blaze::CustomVector<value_type, blaze::unaligned, blaze::unpadded>(&distances[0], np) =
-                blaze::min<rowwise>(blaze::trans(ctrcostmat));
-            total_gain += delta;
-        }
-    }
-    return total_gain;
-}
-#endif
+using coresets::localsearchpp_rounds;
 
-}
-
-}
+} // namespace minocore
 
 
 #endif /* LSEARCHPP_H__ */
