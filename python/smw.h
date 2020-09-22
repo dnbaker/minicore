d99a1f650996c80ee89808f458e915c0b1a60241
dnbh <dnb@cs.jhu.edu>
2020-09-22 15:26:05 -0400

    Correct transposed arguments in SMW constructor

--- python/smw.h
+++ python/smw.h
@@ -69,7 +69,7 @@ public:
         else
             matrix_ = csc2sparse<double>(CSCMatrixView<IpT, IdxT, DataT>(indptr, idx, data, nnz, ydim, xdim));
     }
-    SparseMatrixWrapper(py::object spmat, py::object use_float_py, py::object skip_empty_py) {
+    SparseMatrixWrapper(py::object spmat, py::object skip_empty_py, py::object use_float_py) {
         py::array indices = spmat.attr("indices"), indptr = spmat.attr("indptr"), data = spmat.attr("data");
         py::tuple shape = py::cast<py::tuple>(spmat.attr("shape"));
         const bool use_float = py::cast<bool>(use_float_py), skip_empty = py::cast<bool>(skip_empty_py);
