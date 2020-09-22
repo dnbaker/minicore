eb88939bbc2ad74f81b100ef7ddc10722503e054
dnbh <dnb@cs.jhu.edu>
2020-09-22 14:42:51 -0400

    Update to python installation

--- python/minocore/__init__.py
+++ python/minocore/__init__.py
@@ -1,5 +1,5 @@
 import pyfgc
 from pyfgc import *
 from . import constants
-from .constants import CSR as csr_tuple
+from .constants import CSR as csr_tuple, KMCRSV
 from pyfgc import SparseMatrixWrapper as smw
