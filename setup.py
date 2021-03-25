380be0dd927e4e45a0a874e29d651494a2031e45
dnbh <dnb@cs.jhu.edu>
2021-03-25 17:47:10 -0400

    Remove monkeypatch

--- setup.py
+++
@@ -1,8 +0,0 @@
-from subprocess import check_call
-import sys, os
-
-'''Monkey-patch mapping the setup.py in the base directory to the python/setup.py.
-'''
-wd = os.getcwd()
-print("Working directory: " + wd, file=sys.stderr)
-check_call([sys.executable] + sys.argv)
